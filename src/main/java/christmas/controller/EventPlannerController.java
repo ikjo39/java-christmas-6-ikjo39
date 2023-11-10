477fcd8a43349a3a6f26815c083548d9e5991c4e
ikjo39 <chi4321@naver.com>
2023-11-10 16:45:38 +0900

    feat: MVC 패턴 적용을 위한 controller 생성

---
+++ src/main/java/christmas/controller/EventPlannerController.java
@@ -0,0 +1,17 @@
+package christmas.controller;
+
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
+public class EventPlannerController {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public EventPlannerController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+    }
+}
