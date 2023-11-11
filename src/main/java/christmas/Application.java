5ee54528ab97217973a21782dfba385c0de6ffad
ikjo39 <chi4321@naver.com>
2023-11-11 22:59:21 +0900

    feat: 예외 발생시 반복 처리를 위한 로직 추가
    - 로직 성공까지 입력을 반복하는 로직 추가

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,12 +1,16 @@
 package christmas;
 
 import christmas.controller.EventPlannerController;
+import christmas.handler.ExceptionRetryHandler;
 import christmas.view.InputView;
 import christmas.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        EventPlannerController eventPlannerController = new EventPlannerController(new InputView(), new OutputView());
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        ExceptionRetryHandler retryHandler = new ExceptionRetryHandler(outputView::printExceptionMessage);
+        EventPlannerController eventPlannerController = new EventPlannerController(inputView, outputView, retryHandler);
         eventPlannerController.run();
     }
 }
--- src/main/java/christmas/controller/EventPlannerController.java
+++ src/main/java/christmas/controller/EventPlannerController.java
@@ -1,15 +1,18 @@
 package christmas.controller;
 
+import christmas.handler.ExceptionRetryHandler;
 import christmas.view.InputView;
 import christmas.view.OutputView;
 
 public class EventPlannerController {
     private final InputView inputView;
     private final OutputView outputView;
+    private final ExceptionRetryHandler retryHandler;
 
-    public EventPlannerController(InputView inputView, OutputView outputView) {
+    public EventPlannerController(InputView inputView, OutputView outputView, ExceptionRetryHandler retryHandler) {
         this.inputView = inputView;
         this.outputView = outputView;
+        this.retryHandler = retryHandler;
     }
 
     public void run() {
