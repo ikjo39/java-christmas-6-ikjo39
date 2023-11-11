16ae4bd376bbd4bcbd2ea4fd985c833b396a9c49
ikjo39 <chi4321@naver.com>
2023-11-11 23:44:09 +0900

    feat: 플래너 소개 문구 출력 추가

--- src/main/java/christmas/controller/EventPlannerController.java
+++ src/main/java/christmas/controller/EventPlannerController.java
@@ -16,5 +16,6 @@ public class EventPlannerController {
     }
 
     public void run() {
+        outputView.printPlannerIntroduction();
     }
 }
--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,8 +1,17 @@
 package christmas.view;
 
+import static christmas.constant.OutputMessage.PLANNER_INTRODUCTION;
+
 import christmas.constant.OutputMessage;
 
 public class OutputView {
+    public void printExceptionMessage(String message) {
+        System.out.println(message);
+    }
+
+    public void printPlannerIntroduction() {
+        printOutputMessage(PLANNER_INTRODUCTION);
+    }
 
     private void printOutputMessage(OutputMessage outputMessage) {
         System.out.println(outputMessage.getMessage());
