320fa2b09a1a880067af01ea60cc43442a383914
ikjo39 <chi4321@naver.com>
2023-11-12 00:34:17 +0900

    feat: 메뉴카테고리 enum 및 메뉴 enum 추가

---
+++ src/main/java/christmas/constant/Menu.java
@@ -0,0 +1,50 @@
+package christmas.constant;
+
+import static christmas.constant.MenuCategory.APPETIZER;
+import static christmas.constant.MenuCategory.DESSERT;
+import static christmas.constant.MenuCategory.DRINK;
+import static christmas.constant.MenuCategory.MAIN_DISH;
+
+public enum Menu {
+    MUSHROOM_CREAM_SOUP(APPETIZER, "양송이스프", 6000),
+    TAPAS(APPETIZER, "타파스", 5500),
+    CAESAR_SALAD(APPETIZER, "시저샐러드", 8000),
+
+    T_BONE_STEAK(MAIN_DISH, "티본스테이크", 55000),
+    BBQ_RIB(MAIN_DISH, "바비큐립", 54000),
+    SEAFOOD_SPAGHETTI(MAIN_DISH, "해산물파스타", 35000),
+    CHRISTMAS_PASTA(MAIN_DISH, "크리스마스파스타", 25000),
+
+    CHOCOLATE_CAKE(DESSERT, "초코케이크", 15000),
+    ICE_CREAM(DESSERT, "아이스크림", 5000),
+
+    ZERO_COKE(DRINK, "제로콜라", 3000),
+    RED_WINE(DRINK, "레드와인", 60000),
+    CHAMPAGNE(DRINK, "샴페인", 25000);
+
+    private final MenuCategory menuCategory;
+    private final String name;
+    private final int price;
+
+    Menu(MenuCategory menuCategory, String name, int price) {
+        this.menuCategory = menuCategory;
+        this.name = name;
+        this.price = price;
+    }
+
+    public boolean isMenuDrink() {
+        return menuCategory == DRINK;
+    }
+
+    public MenuCategory getMenuCategory() {
+        return menuCategory;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
---
+++ src/main/java/christmas/constant/MenuCategory.java
@@ -0,0 +1,18 @@
+package christmas.constant;
+
+public enum MenuCategory {
+    APPETIZER("애피타이저"),
+    MAIN_DISH("메인"),
+    DESSERT("디저트"),
+    DRINK("음료");
+
+    private final String name;
+
+    MenuCategory(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
