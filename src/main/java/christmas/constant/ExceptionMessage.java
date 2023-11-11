f097cea76bdd13fe665f8e65d8998b4bee343012
ikjo39 <chi4321@naver.com>
2023-11-12 00:38:13 +0900

    feat: 주문입력 로직 추가
    - 주문입력 요청 문구 추가
    - 적절한 예외메세지 enum 내 추가
    - 입력 구분자로 끝날 경우 예외 추가

--- src/main/java/christmas/constant/ExceptionMessage.java
+++ src/main/java/christmas/constant/ExceptionMessage.java
@@ -1,13 +1,16 @@
 package christmas.constant;
 
 public enum ExceptionMessage {
-    INVALID_VISIT_DAY("유효하지 않은 날짜입니다. 다시 입력해 주세요.");
-    private static final String HEADER = "[ERROR]";
+    INVALID_VISIT_DAY("유효하지 않은 날짜입니다."),
+    INVALID_ORDER("유효하지 않은 주문입니다.");
+
+    private static final String MESSAGE_HEADER = "[ERROR]";
+    private static final String MESSAGE_FOOTER = "다시 입력해 주세요.";
 
     private final String message;
 
     ExceptionMessage(String message) {
-        this.message = String.format("%s %s", HEADER, message);
+        this.message = String.format("%s %s %s", MESSAGE_HEADER, message, MESSAGE_FOOTER);
     }
 
     public String getMessage() {
--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,12 +1,17 @@
 package christmas.view;
 
+import static christmas.constant.ExceptionMessage.INVALID_ORDER;
 import static christmas.constant.ExceptionMessage.INVALID_VISIT_DAY;
 import static christmas.constant.OutputMessage.ENTER_EXPECTED_VISIT_DATE;
+import static christmas.constant.OutputMessage.ENTER_MENU_AND_COUNT_TO_ORDER;
 
 import camp.nextstep.edu.missionutils.Console;
 import christmas.constant.OutputMessage;
+import java.util.Arrays;
+import java.util.List;
 
 public class InputView {
+    private static final String ORDERS_DELIMITER = ",";
 
     public int readVisitDate() {
         printOutputMessage(ENTER_EXPECTED_VISIT_DATE);
@@ -18,6 +23,19 @@ public class InputView {
             return Integer.parseInt(value);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(INVALID_VISIT_DAY.getMessage());
+        }
+    }
+
+    public List<String> readMenuAndCount() {
+        printOutputMessage(ENTER_MENU_AND_COUNT_TO_ORDER);
+        String input = readLine();
+        validateEndWithDelimiter(input);
+        return Arrays.stream(input.split(ORDERS_DELIMITER)).toList();
+    }
+
+    private void validateEndWithDelimiter(String input) {
+        if (input.endsWith(ORDERS_DELIMITER)) {
+            throw new IllegalArgumentException(INVALID_ORDER.getMessage());
         }
     }
 
