ade135318200de5100eec78ec2437d7e467fce95
ikjo39 <chi4321@naver.com>
2023-11-15 18:35:06 +0900

    test: TotalPriceTest 내 엣지 케이스 추가

--- src/test/java/christmas/model/TotalPriceTest.java
+++ src/test/java/christmas/model/TotalPriceTest.java
@@ -8,8 +8,8 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 class TotalPriceTest {
 
-    @DisplayName("모든 이벤트가 활성화 되었는지 여부를 확인한다.")
-    @CsvSource(value = {"9999,false", "10000,true"})
+    @DisplayName("총 주문금액이 10000원 이상이어야 이벤트가 활성화된다.")
+    @CsvSource(value = {"9999,false", "10000,true", "10001,true"})
     @ParameterizedTest
     void isEventEnabled(int given, boolean expected) {
         // given
@@ -22,8 +22,8 @@ class TotalPriceTest {
         assertThat(result).isEqualTo(expected);
     }
 
-    @DisplayName("증정품의 획독 여부를 확인한다.")
-    @CsvSource(value = {"119999,false", "120000,true"})
+    @DisplayName("증정 메뉴 조건의 만족 여부를 확인한다.")
+    @CsvSource(value = {"119999,false", "120000,true", "120001,true"})
     @ParameterizedTest
     void isSatisfiedGiveaway(int given, boolean expected) {
         // given
